7bf159312133ebf603aee9ea0003bbddacb3f752
akash-12398 <napinderbajwa@gmail.com>
2020-09-23 14:53:14 -0500

    first submission

---
+++ RSwansonKWestQuotes/KWest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+namespace RSwansonKWestQuotes
+{   
+    public class QuoteGenerator
+    {
+        private HttpClient _client;
+
+        public QuoteGenerator(HttpClient client)
+        {
+            _client = client;
+        }
+
+          public string KanyeQuote()
+          {
+             var  kanyeURL = "https://api.kanye.rest";
+
+             var KanyeResponse = _client.GetStringAsync(kanyeURL).Result;
+
+            var KanyeQuote = JObject.Parse(KanyeResponse).GetValue("quote").ToString();
+
+            return KanyeQuote;
+          }
+
+        public string RonQuote()
+        {
+           var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+
+            var RonResponse = _client.GetStringAsync(ronURL).Result;
+
+            var ronQuote = JArray.Parse(RonResponse).ToString();
+            return ronQuote;
+
+        }
+
+        public string ChuckQuote()
+        {
+
+            var chuckURL = "https://api.chucknorris.io/jokes/random";
+
+            var ChuckResponse = _client.GetStringAsync(chuckURL).Result;
+
+            var chuckQuote = JObject.Parse(ChuckResponse).GetValue("value").ToString();
+
+            return chuckQuote;
+
+        }
+        
+
+
+    }
+}
+
+
---
+++ RSwansonKWestQuotes/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+namespace RSwansonKWestQuotes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var client = new HttpClient();
+            var quote = new QuoteGenerator(client);
+
+
+            for (int i = 0; i < 5; i++)
+
+
+
+
+
+
+             Console.WriteLine($"Kanye: {quote.KanyeQuote()}");
+
+            Console.WriteLine($"RonQuote: {quote.RonQuote()}");
+
+            Console.WriteLine($"ChuckQuote: {quote.ChuckQuote()}");
+
+
+
+
+
+            
+        }
+
+    }
+
+
+
+       
+}
